6ebc12b23018021c06c796cde2b2440e2b2d9ffc
acheredn <73703578+acheredn@users.noreply.github.com>
2023-01-30 12:51:22 -0600

    test folder and file (re)created

---
+++ test/ballTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class ballTest {
+
+    @Test   
+    public void testCollision() {
+        
+    }
+}
